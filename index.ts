a381620ce97444e4f0a7e33b340dbf3cd6676295
Sayfullah <sayfullah.jumoorty@live.com>
2023-08-23 15:34:16 +0200

    PRAK-39 API route to get image link based on word

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-import express from 'express';
-import rootRoute from './src/rootRoute';
-import userRoute from './src/userRoute';
+import express from "express";
+import rootRoute from "./src/rootRoute";
+import userRoute from "./src/userRoute";
+import imageRoute from "./src/imageRoute";
 
 const app: express.Application = express();
 const port: number = 8080;
 
 // Use the route files as middleware
-app.use('/', rootRoute);
-app.use('/api', userRoute);
+app.use("/", rootRoute);
+app.use("/api", userRoute);
+app.use("/api", imageRoute);
 
 app.listen(port, () => {
   console.log(`TypeScript with Express: http://localhost:${port}/`);
---
+++ src/imageRoute.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import PicScout from "picscout";
+
+const router = express.Router();
+
+router.get("/get/image", async (req, res) => {
+  try {
+    // Fetch the API key from the request headers
+    const userAPIKey = req.headers["x-api-key"];
+
+    // Compare the fetched API key with the stored API key
+    const storedAPIKey = process.env.REACT_APP_PRAKTYK_API_KEY;
+    if (userAPIKey !== storedAPIKey) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // Get image name from the request body
+    const { imageName } = req.body;
+
+    if (!imageName) {
+      return res
+        .status(400)
+        .json({ error: "Image name is required in the request body" });
+    }
+
+    // Append "cartoon" to the image name
+    const modifiedImageName = imageName + "cartoon";
+
+    // Search for the image using PicScout with safe property
+    const picScoutResponse = await PicScout.search(modifiedImageName, {
+      safe: true,
+    });
+
+    // Check if the search was successful
+    if (Array.isArray(picScoutResponse) && picScoutResponse.length > 0) {
+      const imageData = picScoutResponse[0]; // Assuming the first result contains the image data
+      res.json(imageData);
+    } else {
+      res.status(404).json({ error: "Image not found" });
+    }
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    res.status(500).send("Error fetching image");
+  }
+});
+
+export default router;
