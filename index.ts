871fe86e9dde9a721f6a1004a4a294aff0ea7f1b
Muhammed <momosuli8@gmail.com>
2023-08-24 20:50:35 +0200

    PRAK-45 Added Gramar and Vocab Routes

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import rootRoute from "./src/rootRoute";
 import userRoute from "./src/userRoute";
 import imageRoute from "./src/imageRoute";
+import vocabRoute from "./src/vocabRoute";
+import grammarRoute from "./src/grammarRoute";
 
 const app: express.Application = express();
 const port: number = 8080;
@@ -10,6 +12,8 @@ const port: number = 8080;
 app.use("/", rootRoute);
 app.use("/api", userRoute);
 app.use("/api", imageRoute);
+app.use("/api", vocabRoute);
+app.use("/api", grammarRoute);
 
 app.listen(port, () => {
   console.log(`TypeScript with Express: http://localhost:${port}/`);
